95c05ff09d25882891d5514f38a2168a46704452
itwesley <wcshen1126@gmail.com>
2017-02-08 14:04:52 +0800

    first

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main(arg):
+    """ return 1 to 10 number
+
+    >>> main(10)
+    1 2 3 4 5 6 7 8 9
+    """
+
+    for i in range(1, arg):
+        print i,
+
+
+#if __name__ == '__main__':
+#    import doctest
+#    doctest.testmod()
+#    main(10)
